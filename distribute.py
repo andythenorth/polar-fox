cfdc4cbe719e777f9f2f3689c463adc89d94d12a
andythenorth <mail@andythenorth.co.uk>
2018-03-03 16:02:06 +0000

    Change: add Road Hog and Iron Horse consumers

--- distribute.py
+++ distribute.py
@@ -6,7 +6,9 @@ import sys
 import codecs
 from pathlib import Path, PurePosixPath
 
-consumers = [('Unsinkable_Sam', 'unsinkable-sam')]
+consumers = [('Iron_Horse', 'iron-horse'),
+             ('Road_Hog', 'road-hog'),
+             ('Unsinkable_Sam', 'unsinkable-sam')]
 
 def main():
     print("Preparing files for distribution")
@@ -23,6 +25,7 @@ def main():
     dist_file.close()
 
     print("Distributing to downstream consumers")
+    # this assumes that consumers are found in a consistent ../../ location, and have a consistent Project_name/repo-name structure
     consumer_root = os.path.dirname(os.path.dirname(os.path.abspath(currentdir)))
     for consumer in consumers:
         print("..." + consumer[0])
