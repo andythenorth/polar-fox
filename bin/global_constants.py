5eb88c1a889979f39cbfae1d85a38d5283c0ba95
andythenorth <mail@andythenorth.co.uk>
2018-03-27 21:57:28 +0100

    Codechange: stub in some graphics generation hooks

--- bin/global_constants.py
+++ bin/global_constants.py
@@ -1,5 +1,6 @@
 import os
 currentdir = os.curdir
 
+generated_files_path = os.path.join(currentdir, 'generated')
 dist_container_path = os.path.join(currentdir, 'dist')
 dist_package_path = os.path.join(dist_container_path, 'polar_fox')
--- bin/render_cargo_graphics.py
+++ bin/render_cargo_graphics.py
@@ -1,2 +1,25 @@
-print("Render cargo graphics")
-print("[Not implemented]")
+import shutil
+import os
+currentdir = os.curdir
+
+import global_constants
+
+generated_files_path = global_constants.generated_files_path
+cargo_graphics_input_path = os.path.join(currentdir, 'src', 'cargo_graphics')
+cargo_graphics_output_path = os.path.join(generated_files_path, 'cargo_graphics')
+
+def main():
+    print("Render cargo graphics")
+
+    if not os.path.exists(generated_files_path):
+        # don't destroy all of generated, other scripts might have built things into it
+        os.mkdir(generated_files_path)
+    if os.path.exists(cargo_graphics_output_path):
+        # do destroy all of graphics output path
+        shutil.rmtree(cargo_graphics_output_path)
+    os.mkdir(cargo_graphics_output_path)
+
+    print("[DONE]")
+
+if __name__ == '__main__':
+    main()
