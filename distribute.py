6472a3d0cfef5d7e818610933138591de16ae9d3
andythenorth <mail@andythenorth.co.uk>
2018-03-27 10:59:33 +0100

    Change: distribute Polar Fox as a python package, not a module

--- distribute.py
+++ distribute.py
@@ -12,25 +12,32 @@ consumers = [('Iron_Horse', 'iron-horse'),
 
 def main():
     print("Preparing files for distribution")
-    dist_path = os.path.join(currentdir, 'dist')
-    if os.path.exists(dist_path):
-        shutil.rmtree(dist_path)
-    os.mkdir(dist_path)
+    dist_container_path = os.path.join(currentdir, 'dist')
+    dist_package_path = os.path.join(dist_container_path, 'polar_fox')
+    if os.path.exists(dist_container_path):
+        shutil.rmtree(dist_container_path)
+    os.mkdir(dist_container_path)
+    if os.path.exists(dist_package_path):
+        shutil.rmtree(dist_package_path)
+    os.mkdir(dist_package_path)
 
     dist_header = codecs.open(os.path.join(currentdir, 'src', 'dist_header.txt'), 'r','utf8')
-    src_file = codecs.open(os.path.join(currentdir, 'src', 'polar_fox.py'), 'r','utf8')
-    dist_file = codecs.open(os.path.join(dist_path, 'polar_fox.py'), 'w','utf8')
-    dist_file.write(dist_header.read())
-    dist_file.write(src_file.read())
-    dist_file.close()
+    for filename in ['__init__.py', 'constants.py']:
+        src_file = codecs.open(os.path.join(currentdir, 'src', filename), 'r','utf8')
+        dist_file = codecs.open(os.path.join(dist_package_path, filename), 'w','utf8')
+        dist_file.write(dist_header.read())
+        dist_file.write(src_file.read())
+        dist_file.close()
 
     print("Distributing to downstream consumers")
     # this assumes that consumers are found in a consistent ../../ location, and have a consistent Project_name/repo-name structure
     consumer_root = os.path.dirname(os.path.dirname(os.path.abspath(currentdir)))
     for consumer in consumers:
         print("..." + consumer[0])
-        consumer_dst_path = os.path.join(consumer_root, consumer[0], consumer[1], 'src', 'polar_fox.py')
-        shutil.copy(os.path.join(dist_path, 'polar_fox.py'), consumer_dst_path)
+        consumer_dst_path = os.path.join(consumer_root, consumer[0], consumer[1], 'src', 'polar_fox')
+        if os.path.exists(consumer_dst_path):
+            shutil.rmtree(consumer_dst_path)
+        shutil.copytree(os.path.join(dist_package_path), consumer_dst_path)
 
     print("[DONE]")
     print("Don't forget to test and commit changes for each consumer")
