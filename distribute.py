e6409ca1b8228230e38725e629219e1cf6142fee
andythenorth <mail@andythenorth.co.uk>
2018-03-27 11:25:15 +0100

    Change: distribute cargo graphics files

--- distribute.py
+++ distribute.py
@@ -21,13 +21,17 @@ def main():
         shutil.rmtree(dist_package_path)
     os.mkdir(dist_package_path)
 
-    dist_header = codecs.open(os.path.join(currentdir, 'src', 'dist_header.txt'), 'r','utf8')
+    dist_file_header = codecs.open(os.path.join(currentdir, 'src', 'dist_file_header.txt'), 'r','utf8').read()
     for filename in ['__init__.py', 'constants.py', 'pixa.py']:
         src_file = codecs.open(os.path.join(currentdir, 'src', filename), 'r','utf8')
         dist_file = codecs.open(os.path.join(dist_package_path, filename), 'w','utf8')
-        dist_file.write(dist_header.read())
+        dist_file.write(dist_file_header)
         dist_file.write(src_file.read())
         dist_file.close()
+    for dir_name in ['cargo_graphics']:
+        dist_dir_path =  os.path.join(dist_package_path, dir_name)
+        shutil.copytree(os.path.join(currentdir, 'src', dir_name), dist_dir_path)
+        shutil.copy(os.path.join(currentdir, 'src', 'dist_dir_header.txt'), os.path.join(dist_dir_path, '_files_here_are_generated.txt'))
 
     print("Distributing to downstream consumers")
     # this assumes that consumers are found in a consistent ../../ location, and have a consistent Project_name/repo-name structure
