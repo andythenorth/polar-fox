76ffeea832cceacc017e61a980a0781d704c8250
andythenorth <mail@andythenorth.co.uk>
2018-10-04 09:32:42 +0100

    Change: GenerateAdditionalSpritesheet now saves the additional spritesheet

--- src/graphics_units.py
+++ src/graphics_units.py
@@ -138,13 +138,15 @@ class GenerateAdditionalSpritesheet(ProcessingUnit):
         Creates and saves an additional spritesheet using the passed filename and an image for content.
         Used when it's simpler to use a separate spritesheet with in-game sprite compositing.
         Returns the original spritesheet unchanged, so can be used anywhere in a pipeline.
+        Arguably this could have just been done in the pipeline, but it's cleaner to do it in the pipeline as a unit.
     """
-    def __init__(self):
-        print('GenerateAdditionalSpritesheet.init() called')
+    def __init__(self, additional_spritesheet, output_path):
+        self.additional_spritesheet = additional_spritesheet
+        self.output_path = output_path
         super().__init__()
 
     def render(self, spritesheet):
-        print('GenerateAdditionalSpritesheet.render() called')
+        self.additional_spritesheet.save(self.output_path)
         return spritesheet
 
 
