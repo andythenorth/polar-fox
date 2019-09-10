da7a999c1c1d4291ba58e1af1bf523dc3d7d0cc1
andythenorth <mail@andythenorth.co.uk>
2019-09-10 21:25:27 +0100

    Codechange: clean up a var name

--- bin/render_cargo_graphics.py
+++ bin/render_cargo_graphics.py
@@ -13,34 +13,34 @@ generated_files_path = global_constants.generated_files_path
 
 DOS_PALETTE = Image.open('palette_key.png').palette
 
-piece_cargo_graphics_maps = [("tarps_1", "tarps_blue_1",
-                             {136: 145, 137: 146, 138: 147, 139: 148,
-                             140: 149, 141: 150, 142: 151, 143: 152}),
-                             ("tarps_1", "tarps_2cc_1",
-                             {136: 198, 137: 199, 138: 200, 139: 201,
-                             140: 202, 141: 203, 142: 204, 143: 205}),
-                             ("tarps_1", "tarps_gold_1",
-                             {136: 60, 137: 61, 138: 62, 139: 63,
-                             140: 64, 141: 65, 142: 66, 143: 67}),
-                             ("tarps_1", "tarps_red_1",
-                             {136: 40, 137: 41, 138: 42, 139: 43,
-                             140: 44, 141: 45, 142: 46, 143: 47}),
-                             ("barrels_silver", "barrels_silver", {}),
-                             ("coffee", "coffee", {}),
-                             ("crates_1", "crates_1", {}),
-                             ("fruit", "fruit", {}),
-                             ("logs", "logs", {}),
-                             ("lumber_planks", "lumber_planks", {}),
-                             ("nuts", "nuts", {}),
-                             ("coils_1", "paper_coils",
-                             {136: 8, 137: 9, 138: 10, 139: 11,
-                             140: 12, 141: 13, 142: 14, 143: 15}),
-                             ("coils_1", "steel_coils",
-                             {136: 3, 137: 16, 138: 17, 139: 18,
-                             140: 19, 141: 20, 142: 21, 143: 22}),
-                             ("coils_1", "copper_coils",
-                             {136: 60, 137: 112, 138: 62, 139: 115,
-                             140: 117, 141: 118, 142: 119, 143: 120})]
+cargo_graphics_maps = [("tarps_1", "tarps_blue_1",
+                       {136: 145, 137: 146, 138: 147, 139: 148,
+                       140: 149, 141: 150, 142: 151, 143: 152}),
+                       ("tarps_1", "tarps_2cc_1",
+                       {136: 198, 137: 199, 138: 200, 139: 201,
+                       140: 202, 141: 203, 142: 204, 143: 205}),
+                       ("tarps_1", "tarps_gold_1",
+                       {136: 60, 137: 61, 138: 62, 139: 63,
+                       140: 64, 141: 65, 142: 66, 143: 67}),
+                       ("tarps_1", "tarps_red_1",
+                       {136: 40, 137: 41, 138: 42, 139: 43,
+                       140: 44, 141: 45, 142: 46, 143: 47}),
+                       ("barrels_silver", "barrels_silver", {}),
+                       ("coffee", "coffee", {}),
+                       ("crates_1", "crates_1", {}),
+                       ("fruit", "fruit", {}),
+                       ("logs", "logs", {}),
+                       ("lumber_planks", "lumber_planks", {}),
+                       ("nuts", "nuts", {}),
+                       ("coils_1", "paper_coils",
+                       {136: 8, 137: 9, 138: 10, 139: 11,
+                       140: 12, 141: 13, 142: 14, 143: 15}),
+                       ("coils_1", "steel_coils",
+                       {136: 3, 137: 16, 138: 17, 139: 18,
+                       140: 19, 141: 20, 142: 21, 143: 22}),
+                       ("coils_1", "copper_coils",
+                       {136: 60, 137: 112, 138: 62, 139: 115,
+                       140: 117, 141: 118, 142: 119, 143: 120})]
 
 # eh note that it's CC1 not 1CC here because python var names can't start with digit, but 1CC everywhere else :P
 CC1 = 198
@@ -93,17 +93,17 @@ def main():
         os.mkdir(generated_files_path)
 
     # this is a bit crude, but eh, if it works it works
-    for graphics_type_path, cargo_graphics_maps in {'cargo_graphics': piece_cargo_graphics_maps, 'intermodal_container_graphics': intermodal_container_graphics_maps}.items():
+    for graphics_type_path, graphics_maps in {'cargo_graphics': cargo_graphics_maps, 'intermodal_container_graphics': intermodal_container_graphics_maps}.items():
         graphics_input_path = os.path.join(currentdir, 'src', graphics_type_path)
         graphics_output_path = os.path.join(generated_files_path, graphics_type_path)
         if os.path.exists(graphics_output_path):
             # do destroy all of graphics output path
             shutil.rmtree(graphics_output_path)
         os.mkdir(graphics_output_path)
-        for cargo_graphics_map in cargo_graphics_maps:
-            input_image = Image.open(os.path.join(graphics_input_path, cargo_graphics_map[0] + '.png')).crop((0, 0, 300, 440))
-            units = [SimpleRecolour(knockout_guides_map), SimpleRecolour(cargo_graphics_map[2])]
-            result = render(cargo_graphics_map[1], input_image, units, graphics_output_path)
+        for graphics_map in graphics_maps:
+            input_image = Image.open(os.path.join(graphics_input_path, graphics_map[0] + '.png')).crop((0, 0, 300, 440))
+            units = [SimpleRecolour(knockout_guides_map), SimpleRecolour(graphics_map[2])]
+            result = render(graphics_map[1], input_image, units, graphics_output_path)
 
     print("[DONE]")
 
