73941173056027dbd72c2c15cb91391f4f1fe91f
Andy <andy@teamrubber.com>
2020-11-29 13:16:45 +0000

    Fix: default cargo for 'hopper' should be coal, swap with 'rock_hopper' definition

--- src/constants.py
+++ src/constants.py
@@ -146,7 +146,7 @@ disallowed_refits_by_label = {
 # vehicle classes can also just provide their own list locally, using this is convenient, not obligatory
 default_cargos = {
                   'box': ['GOOD', 'VPTS', 'FOOD'],
-                  'coal_hopper': ['COAL', 'COKE', 'NITR', 'POTA'],
+                  'hopper': ['COAL', 'COKE', 'NITR', 'POTA'],
                   'cryo_gases': ['O2__', 'CHLO'],
                   'covered_hopper': ['SAND', 'KAOL', 'PLAS'],
                   'dump': ['MNO2', 'FECR', 'NITR', 'PHOS', 'SAND', 'GRVL'],
@@ -157,7 +157,7 @@ default_cargos = {
                   'flat': ['STEL', 'STCB', 'COPR', 'METL'],
                   'fruit_veg': ['FRUT', 'BEAN', 'CASS', 'JAVA', 'NUTS'],
                   'grain_hopper': ['GRAI', 'PLAS'],
-                  'hopper': ['LIME', 'GRVL', 'SAND', 'PORE', 'SALT', 'IORE', 'CORE'],
+                  'rock_hopper': ['LIME', 'GRVL', 'SAND', 'PORE', 'SALT', 'IORE', 'CORE'],
                   # intermodal, uses box
                   'mail': ['MAIL'],
                   'metal': ['STEL', 'COPR'],
